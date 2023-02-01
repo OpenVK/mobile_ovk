d47c239d1dc6859a66d24441fee946dace387c46
veselcraft <veselcraft@icloud.com>
2023-02-01 21:00:05 +0300

    likes without page reload, fix >1024px setup, add pull sup
    Co-authored-by: Daniel <60743585+myslivets@users.noreply.github.com>

--- res/podrobnee.js
+++ res/podrobnee.js
@@ -5,4 +5,24 @@ p_bt.addEventListener('click', function(){
 	let text = ["Показать подробнее", "Скрыть"];
 	p_bt.innerText = text[Number(p_bt.innerText === text[0])]
 	p_content.classList.toggle('active');
-})
+});
+
+u(".post-like-button").on("click", function(e) {
+    e.preventDefault();
+    
+    var thisBtn = u(this).first();
+    var link    = u(this).attr("href");
+    var heart   = u(".like", thisBtn);
+    var counter = u(".likeCnt", thisBtn);
+    var likes   = counter.text() === "" ? 0 : counter.text();
+    var isLiked = heart.attr("id") === 'liked';
+    
+    ky(link);
+    heart.attr("id", isLiked ? '' : 'liked');
+    counter.text(parseInt(likes) + (isLiked ? -1 : 1));
+    if (counter.text() === "0") {
+        counter.text("");
+    }
+    
+    return false;
+});
